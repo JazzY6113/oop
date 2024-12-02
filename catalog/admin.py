f61c1dd60e9f1e78335727ebec37f0c46ea9c4a1
JazzY6113 <kbogomolov22@gmail.com>
2024-12-02 10:49:29 +0300

    Управление отображаемыми и вложенными полями

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,6 +3,7 @@ from .models import Author, Genre, Book, BookInstance
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # admin.site.register(Author)
 admin.site.register(Genre)
@@ -16,4 +17,4 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
