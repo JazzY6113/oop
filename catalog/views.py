7db07f72226ef7af06366c1409c7605e7c4d0352
JazzY6113 <kbogomolov22@gmail.com>
2024-12-02 21:30:51 +0300

    Сессии

--- catalog/views.py
+++ catalog/views.py
@@ -12,25 +12,19 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
-
     num_genres = Genre.objects.count()
-
     search_word = request.GET.get('search_word', '')
-
     num_books_with_word = Book.objects.filter(title__icontains=search_word).count() if search_word else 0
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     return render(
         request,
         'index.html',
-        context={
-            'num_books': num_books,
-            'num_instances': num_instances,
-            'num_instances_available': num_instances_available,
-            'num_authors': num_authors,
-            'num_genres': num_genres,
-            'num_books_with_word': num_books_with_word,
-            'search_word': search_word,
-        },
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},  # num_visits appended
     )
 
 class BookListView(generic.ListView):
